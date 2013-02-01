81679bff3e177c1f17a392f66ec97def5e212e32
j-delacruz <jdelacruz@appsource.biz>
2013-02-01 21:56:07 +0800

    add restriction on admins_only views

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(:version => 20130201095700) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",    :null => false
+    t.string   "encrypted_password",     :default => "",    :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -31,9 +31,9 @@ ActiveRecord::Schema.define(:version => 20130201095700) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.boolean  "admin"
+    t.datetime "created_at",                                :null => false
+    t.datetime "updated_at",                                :null => false
+    t.boolean  "admin",                  :default => false
     t.integer  "agency_id"
   end
 
