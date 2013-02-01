13d0d35a42a1823da696b8fe435687bb968255e4
j-delacruz <jdelacruz@appsource.biz>
2013-02-01 17:43:58 +0800

    Crud for agency

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Gppbv2::Application.routes.draw do
 
   devise_for :users
 
+  resources :agencies
+
   root to: 'pages#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
