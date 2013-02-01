3d40ab90eea71a4200d24bae90da9ea6bb468264
j-delacruz <jdelacruz@appsource.biz>
2013-02-01 16:45:02 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gppbv2::Application.config.secret_token = '527814727b6c8e93f46ce2c346ed4336efe48e9d0183b781387ca412423dd368b4fc5434b7c7cf2d72b6c20b09543338857d0a11adaa078418ab2faa3feae3f7'
