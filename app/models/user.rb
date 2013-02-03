05cda09f218f5477199d1059f73f7da0f76ff115
j-delacruz <jdelacruz@appsource.biz>
2013-02-03 12:57:55 +0800

    add on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :agency_id
   # attr_accessible :title, :body
+  
 
   belongs_to :agency
 end
