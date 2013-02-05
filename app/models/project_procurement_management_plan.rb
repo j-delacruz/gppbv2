0fcbc2cf49d7ba8c010f652f67c1a20a8a10d5cc
shielasalamat <ssalamat@appsource.biz>
2013-02-05 17:28:20 +0800

    adds generate buttons, print button, export buttons

--- app/models/project_procurement_management_plan.rb
+++ app/models/project_procurement_management_plan.rb
@@ -1,5 +1,6 @@
 class ProjectProcurementManagementPlan < ActiveRecord::Base
-  attr_accessible :agency_id, :name, :user_id, :projects_attributes, :year, :version_num, :description
+  attr_accessible :agency_id, :name, :user_id, :projects_attributes, :year, :version_num, :description,
+                  :ppmp_period_covered_start, :ppmp_period_covered_end
   belongs_to :agency
   belongs_to :user
   has_many :projects, dependent: :destroy
@@ -8,4 +9,6 @@ class ProjectProcurementManagementPlan < ActiveRecord::Base
   def ppmp_form_name
     "#{name} # #{version_num} of #{year}"
   end
+  
+ 
 end
