3e9087b8a32d504261d8107902382ba14f50cd46
j-delacruz <jdelacruz@appsource.biz>
2013-02-06 14:47:20 +0800

    implement security-code successfull

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,24 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :agency_id, :first_name, :last_name, :username
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :admin, :agency_id, :first_name, :last_name, :username, :agency_code
   # attr_accessible :title, :body
+  attr_accessor :agency_code
   has_many :project_procurement_management_plans
   belongs_to :agency
- # validates :username, :uniqueness=> true
+  
+  validates :username, uniqueness: true
+  validate :validate_agency_code
 
- def fullname
+  def fullname
    "#{first_name} #{last_name}"
- end
+  end
+
+    def validate_agency_code
+       if agency && agency.security_code != agency_code
+          errors.add(:base, "Error message")
+       end
+    end
+
+ 
 end
