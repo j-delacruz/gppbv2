894f17cfe6adca849afada13fc0e9f731a47929c
j-delacruz <jdelacruz@appsource.biz>
2013-02-06 14:03:39 +0800

    modify seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 #
 users = []
-user1 = User.create!(email: 'admin@example.com', password: 'password', admin: true, first_name: 'Admin', last_name: 'Admin', :username=>'admin' )
+user1 = User.create!(email: 'admin@example.com', password: 'password', admin: true, first_name: 'Admin', last_name: 'Admin', username: 'Admin' )
 puts user1.email
 
 agency0 = Agency.create!(name: 'Admin', security_code: 'HKT3FD56R1')
@@ -15,11 +15,11 @@ agency1 = Agency.create!(name: 'James Agency', security_code: 'AGENCY0001')
 agency2 = Agency.create!(name: 'Shiela Agency', security_code: 'AGENCY0002')
 agency3 = Agency.create!(name: 'Vieve Agency', security_code: 'AGENCY0003')
 
-user2 = User.create!(email: 'james@example.com', password: 'password', agency_id: 1, first_name: 'James', last_name: 'Dela Cruz', :username=>'jdelacruz')
+user2 = User.create!(email: 'james@example.com', password: 'password', agency_id: 2, first_name: 'James', last_name: 'Dela Cruz', username: 'jdelacruz')
 puts user2.email
 
-user3 = User.create!(email: 'shiela@example.com', password: 'password', agency_id: 2, first_name: 'Shiela', last_name: 'Salamat', :username=>'ssalamat')
+user3 = User.create!(email: 'shiela@example.com', password: 'password', agency_id: 3, first_name: 'Shiela', last_name: 'Salamat', username: 'ssalamat')
 puts user3.email
 
-user4 = User.create!(email: 'vieve@example.com', password: 'password', agency_id: 3, first_name: 'Vieve', last_name: 'Pablo', :username=>'vpablo')
+user4 = User.create!(email: 'vieve@example.com', password: 'password', agency_id: 4, first_name: 'Vieve', last_name: 'Pablo', username: 'vpablo')
 puts user4.email
